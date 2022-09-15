780568a334f62c9b2ff19c413a554afa3e0e772b
Tero Keränen <tero-keranen@hotmail.com>
2022-09-15 19:41:21 +0300

    adding nav bar

--- app.js
+++ app.js
@@ -169,3 +169,19 @@ function scrollTop () {
         behavior : 'smooth',
     })
 }
+
+/*
+//////////////////
+Navbar movement
+///////////////
+*/
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+
+
+navToggle.addEventListener('click', function () {
+
+    links.classList.toggle('show-links');
+})
